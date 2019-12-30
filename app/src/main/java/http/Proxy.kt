119f6a4c9012e390695db53eb6b36a0a5cd0e6f0
gwh2 <garywhancock@yahoo.com>
2019-12-30 13:31:32 -0700

    fixed protocol?

--- app/src/main/java/http/Proxy.kt
+++ app/src/main/java/http/Proxy.kt
@@ -7,6 +7,7 @@ import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.SocketTimeoutException
 import java.net.URL
+import java.nio.charset.StandardCharsets
 
 object Proxy {
 
@@ -16,9 +17,9 @@ object Proxy {
 
         val url = URL(validatedURLString)
         val http : HttpURLConnection = url.openConnection() as HttpURLConnection
-        http.requestMethod = "GET" //"POST" ???
-        http.doOutput = false
-        http.connectTimeout = 8000 //8 second timout
+        http.requestMethod = "POST"
+        http.doOutput = true
+
         try{
             http.connect()
         }
@@ -26,13 +27,12 @@ object Proxy {
             return "connection timed out..."
         }
 
-        if(http.responseCode == HttpURLConnection.HTTP_OK){
+        return if(http.responseCode == HttpURLConnection.HTTP_OK){
             val responseBody = http.inputStream
             val responseData = readString(responseBody)
-            return responseData
-        }
-        else{
-            return "Error: returned with status code " + http.responseCode + " :: " + http.responseMessage
+            responseData
+        } else{
+            "Error: returned with status code " + http.responseCode + " :: " + http.responseMessage
         }
     }
 
@@ -48,18 +48,14 @@ object Proxy {
             if(len <= 0) break
             sb.append(buf, 0, len)
         } while(len > 0)
-//        while ((len = sr.read(buf)) > 0) {
-//            sb.append(buf, 0, len)
-//        }
         return sb.toString()
     }
 }
 
 private fun String.validateURL(): String {
-//    if(!this.contains(".")) return "ERROR: invalid url"
     if(this.toLowerCase() == "todo") return "ERROR: invalid url"
     val newURL = if(this.startsWith("http")) this
-    else "https://$this"
+    else "http://$this"
     try{
         URL(newURL)
     } catch (mue: MalformedURLException){
